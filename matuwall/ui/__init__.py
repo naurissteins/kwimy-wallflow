96504012f41fc893824101cd7dca308ef5c7a061
naurissteins <me@naurissteins.com>
2026-02-25 20:34:50 +0200

    [code] addded explicit exports for all UI mixins

--- matuwall/ui/__init__.py
+++ matuwall/ui/__init__.py
@@ -1,1 +1,21 @@
 """UI helpers for Matuwall."""
+
+from .bootstrap import AppBootstrapMixin
+from .content import ContentMixin
+from .navigation import NavigationMixin
+from .panel import PanelMixin
+from .runtime import RuntimeMixin
+from .thumbnails import ThumbnailMixin
+from .window_setup import WindowSetupMixin
+from .window_state import WindowStateMixin
+
+__all__ = [
+    "AppBootstrapMixin",
+    "ContentMixin",
+    "NavigationMixin",
+    "PanelMixin",
+    "RuntimeMixin",
+    "ThumbnailMixin",
+    "WindowSetupMixin",
+    "WindowStateMixin",
+]
